48705ce5fe3b832de4a0f728b554f9be743fee59
Delbar <clement.delbar@insa-cvl.fr>
2016-02-16 15:20:17 +0100

    Installation de Doctrine

--- src/index.php
+++ src/index.php
@@ -1,3 +1,20 @@
 <?php
-include_once("../config/database.config.php");
+require_once('../vendor/autoload.php');
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+$paths = array("../src/");
+$isDevMode = false;
+
+// the connection configuration
+$dbParams = array(
+    'driver'   => 'pdo_mysql',
+    'user'     => 'projet-appli',
+    'password' => 'projet-appli',
+    'dbname'   => 'projet_appli',
+);
+
+$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+$entityManager = EntityManager::create($dbParams, $config);
 ?>
